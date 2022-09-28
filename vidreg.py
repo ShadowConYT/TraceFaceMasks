6da8001611dc14b50a45a0c200d443268272afe2
ShadowCon <111366035+ShadowConYT@users.noreply.github.com>
2022-09-29 05:12:15 +0530

    Update vidreg.py

--- vidreg.py
+++ vidreg.py
@@ -8,7 +8,7 @@ pTime = 0
 mpDraw = mp.solutions.drawing_utils
 mpFaceMesh = mp.solutions.face_mesh
 faceMesh = mpFaceMesh.FaceMesh(max_num_faces=2)
-drawSpec = mpDraw.DrawingSpec(thickness=1, circle_radius=2)
+drawspec = mpDraw.DrawingSpec(thickness=1, circle_radius=1)
 
 while True:
     success, img = cap.read()
@@ -16,18 +16,20 @@ while True:
     results = faceMesh.process(imgRGB)
     if results.multi_face_landmarks:
         for faceLms in results.multi_face_landmarks:
-            mpDraw.draw_landmarks(img, faceLms, mpFaceMesh.FACE_CONNECTIONS,
-                                  drawSpec,drawSpec)
+            mpDraw.draw_landmarks(img, faceLms, mpFaceMesh.FACEMESH_CONTOURS, drawspec, drawspec)
             for id,lm in enumerate(faceLms.landmark):
-                #print(lm)
+                print(lm)
                 ih, iw, ic = img.shape
-                x,y = int(lm.x*iw), int(lm.y*ih)
+                x, y = int(lm.x*iw), int(lm.y*ih)
                 print(id, x, y)
-
     cTime = time.time()
     fps = 1 / (cTime - pTime)
     pTime = cTime
     cv2.putText(img, f'FPS: {int(fps)}', (20, 70), cv2.FONT_HERSHEY_PLAIN,
                 3, (255, 0, 0), 3)
-    cv2.imshow("Image", img)
+    cv2.namedWindow("Resized_Window", cv2.WINDOW_NORMAL)
+    cv2.resizeWindow("Resized_Window", 1280, 720)
+    #cv2.imshow("Image", img)
+    cv2.imshow("Resized_Window", img)
+
     cv2.waitKey(1)
